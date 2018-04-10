f59f1b8c2a8a126379b3dc35a430a64833107aa3
venkatesh <vs498@njit.edu>
2018-04-09 21:56:09 -0400

    Added answers  migrate, model and made answer seeder and added test for  question(clean up), and answer test

--- app/User.php
+++ app/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Question');
     }
+
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
 }
+
+
+
